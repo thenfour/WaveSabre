7b2e90b9d786d36a4fb16c5c1ac2a892ec086c47
Carl Corcoran <carlco@gmail.com>
2025-08-16 21:26:30 +0200

    modernizing project gen & cmake a bit - checking if cmake config error - specifying .net 4.7.2 correctly - don't try to post-build copy non-plugins

--- switch_to_x86.cmd
+++ switch_to_x86.cmd
@@ -2,13 +2,21 @@ pushd %~dp0
 
 
 @echo Make sure visual studio is closed, and press a key
+@echo ** NOTE: your vst plugins dir should be:
+@echo C:\root\vstpluginlinks\WaveSabre
 
 @pause
 
 
-del build\CMakeCache.txt
+del build\CMakeCache.txt > nul
 
-cmake -DVSTDIR=C:\root\vstpluginlinks\WaveSabre -B build -A Win32
+cmake -S . -DVSTDIR=C:\root\vstpluginlinks\WaveSabre -B build -A Win32
+
+if %ERRORLEVEL% NEQ 0 (
+    @echo CMake configuration failed
+    @pause
+    exit /b 1
+)
 
 @echo hit a key to open visual studio...
 
