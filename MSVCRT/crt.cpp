9dfb2ea138a692a770a8017a9a222fcb2d77ddfc
tenfour <carlco@gmail.com>
2026-03-01 11:49:51 +0100

    it builds

--- MSVCRT/crt.cpp
+++ MSVCRT/crt.cpp
@@ -6,3 +6,23 @@ void __cdecl operator delete[](void* p) {
 void* __cdecl operator new[](size_t bytes) {
 	return malloc(bytes);// return HeapAlloc(GetProcessHeap(), 0, bytes);
 }
+
+#if defined(_M_IX86)
+extern "C" long __declspec(naked) __cdecl _ftol2()
+{
+	__asm {
+		sub esp, 4
+		fistp dword ptr [esp]
+		pop eax
+		ret
+	}
+}
+
+extern "C" long __declspec(naked) __cdecl _ftol2_sse()
+{
+	__asm {
+		cvttss2si eax, xmm0
+		ret
+	}
+}
+#endif
