18de166ee25d22457e60b7bbed6eaaa4438f4f94
tenfour <carlco@gmail.com>
2026-03-16 15:34:08 +0100

    #156 WIP making the rotation range consistent with UI, fixing source channel ranges

--- WaveSabreCore/Devices/Maj7Width.hpp
+++ WaveSabreCore/Devices/Maj7Width.hpp
@@ -1,152 +1,155 @@
 #pragma once
 
-#include "../WSCore/Device.h"
-#include "../GigaSynth/Maj7Basic.hpp"
-#include "../Filters/FilterOnePole.hpp"
 #include "../Analysis/StereoImageAnalysis.hpp"
+#include "../Filters/FilterOnePole.hpp"
+#include "../GigaSynth/Maj7Basic.hpp"
+#include "../WSCore/Device.h"
+
 
 namespace WaveSabreCore
 {
-	struct Maj7Width : public Device
-	{
-		// roughly in order of processing,
-		enum class ParamIndices
-		{
-			LeftSource, // 0 = left, 1 = right
-			RightSource, // 0 = left, 1 = right
-			RotationAngle,
-			SideHPFrequency,
-			MidSideBalance,
-			Pan,
-			OutputGain,
+struct Maj7Width : public Device
+{
+  // roughly in order of processing,
+  enum class ParamIndices
+  {
+    LeftSource,   // 0 = left, 1 = right
+    RightSource,  // 0 = left, 1 = right
+    RotationAngle,
+    SideHPFrequency,
+    MidSideBalance,
+    Pan,
+    OutputGain,
 
-			NumParams,
-		};
+    NumParams,
+  };
 
-		// NB: max 8 chars per string.
-#define MAJ7WIDTH_PARAM_VST_NAMES(symbolName) static constexpr char const* const symbolName[(int)::WaveSabreCore::Maj7Width::ParamIndices::NumParams]{ \
-	{"LSrc"},\
-	{"RSrc"},\
-	{"Rot"},\
-	{"SideHPF"},\
-	{"MSBal"},\
-	{"Pan"},\
-	{"OutGain"},\
-}
+  // NB: max 8 chars per string.
+#define MAJ7WIDTH_PARAM_VST_NAMES(symbolName)                                                                          \
+  static constexpr char const* const symbolName[(int)::WaveSabreCore::Maj7Width::ParamIndices::NumParams]              \
+  {                                                                                                                    \
+    {"LSrc"}, {"RSrc"}, {"Rot"}, {"SideHPF"}, {"MSBal"}, {"Pan"}, {"OutGain"},                                         \
+  }
 
-		static constexpr M7::VolumeParamConfig gVolumeCfg{ 3.9810717055349722f, 12.0f };
+  static constexpr M7::VolumeParamConfig gVolumeCfg{3.9810717055349722f, 12.0f};
 
-		float mParamCache[(int)ParamIndices::NumParams];
-		M7::ParamAccessor mParams;
+  float mParamCache[(int)ParamIndices::NumParams];
+  M7::ParamAccessor mParams;
 
-static_assert((int)ParamIndices::NumParams == 7, "param count probably changed and this needs to be regenerated.");
-static constexpr int16_t gParamDefaults[(int)ParamIndices::NumParams] = {
-  -32767, // LSrc = -1
-  32767, // RSrc = 1
-  16383, // Rot = 0.5
-  0, // SideHPF = 0
-  0, // MSBal = 0
-  0, // Pan = 0
-  16422, // OutGain = 0.50118720531463623047
+  static_assert((int)ParamIndices::NumParams == 7, "param count probably changed and this needs to be regenerated.");
+  static constexpr int16_t gParamDefaults[(int)ParamIndices::NumParams] = {
+      -32767,  // LSrc = -1
+      32767,   // RSrc = 1
+      16383,   // Rot = 0.5
+      0,       // SideHPF = 0
+      0,       // MSBal = 0
+      0,       // Pan = 0
+      16422,   // OutGain = 0.50118720531463623047
+  };
+
+  M7::MoogOnePoleFilter mFilter;
+
+#ifdef SELECTABLE_OUTPUT_STREAM_SUPPORT
+  AnalysisStream mInputAnalysis[2];
+  AnalysisStream mOutputAnalysis[2];
+  StereoImagingAnalysisStream mInputImagingAnalysis;
+  StereoImagingAnalysisStream mOutputImagingAnalysis;
+#endif  // SELECTABLE_OUTPUT_STREAM_SUPPORT
+
+  Maj7Width()
+      : Device((int)ParamIndices::NumParams, mParamCache, gParamDefaults)
+      , mParams(mParamCache, 0)
+#ifdef SELECTABLE_OUTPUT_STREAM_SUPPORT
+      ,
+      // quite fast moving here because the user's looking for transients.
+      mInputAnalysis{AnalysisStream{1200}, AnalysisStream{1200}}
+      , mOutputAnalysis{AnalysisStream{1200}, AnalysisStream{1200}}
+#endif  // SELECTABLE_OUTPUT_STREAM_SUPPORT
+  {
+    LoadDefaults();
+  }
+
+#ifdef MAJ7WIDTH_FULL_FEATURE
+  // 2D rotation around origin
+  void Rotate2(float& l, float& r, ParamIndices param)
+  {
+    // https://www.musicdsp.org/en/latest/Effects/255-stereo-field-rotation-via-transformation-matrix.html
+    //r = rotation_angle
+    float rot = mParams.GetScaledRealValue(param, -M7::math::gPIHalf, M7::math::gPIHalf, 0);
+    float s = M7::math::sin(rot);
+    float c = M7::math::cos(rot);
+    float t = l * c - r * s;
+    r = l * s + r * c;
+    l = t;
+  }
+#endif  // MAJ7WIDTH_FULL_FEATURE
+
+  virtual void Run(float** inputs, float** outputs, int numSamples) override
+  {
+    auto panGains = M7::math::PanToFactor(mParams.GetN11Value(ParamIndices::Pan, 0));
+    mFilter.SetParams(M7::FilterCircuit::OnePole,
+                      M7::FilterSlope::Slope6dbOct,
+                      M7::FilterResponse::Highpass,
+                      mParams.GetFrequency(ParamIndices::SideHPFrequency, M7::gFilterFreqConfig),
+                      M7::Param01{0} /*reso*/,
+                      0 /*gain*/);
+    float masterLinearGain = mParams.GetLinearVolume(ParamIndices::OutputGain, gVolumeCfg) *
+                             M7::math::gPanCompensationGainLin;
+
+    for (size_t i = 0; i < (size_t)numSamples; ++i)
+    {
+      float l = inputs[0][i];
+      float r = inputs[1][i];
+
+      float leftSrcN11 = mParams.GetN11Value(ParamIndices::LeftSource, 0);
+      float rightSrcN11 = mParams.GetN11Value(ParamIndices::RightSource, 0);
+
+      float left = M7::math::lerp(l, r, leftSrcN11 * 0.5f + 0.5f);    // rescale from -1,1 to 0,1 for lerp
+      float right = M7::math::lerp(l, r, rightSrcN11 * 0.5f + 0.5f);  // rescale from -1,1 to 0,1 for lerp
+
+#ifdef MAJ7WIDTH_FULL_FEATURE
+      Rotate2(left, right, ParamIndices::RotationAngle);
+#endif  // MAJ7WIDTH_FULL_FEATURE
+
+      auto ms = M7::FloatPair{left, right}.MSEncode();
+
+      // hp on side channel
+      ms.x[1] = mFilter.ProcessSample(ms.Side());
+      float msbal = mParams.GetN11Value(ParamIndices::MidSideBalance, 0);
+      if (msbal < 0)
+      {
+        // reduce side
+        ms.x[1] *= msbal + 1.0f;
+      }
+      if (msbal > 0)
+      {
+        ms.x[0] *= 1.0f - msbal;
+      }
+
+      auto outputPrePan = ms.MSDecode() * masterLinearGain;
+      auto output = outputPrePan.mul(panGains);
+
+      //M7::MSDecode(mid, side, &left, &right);
+      //left *= gains.x[0] * masterLinearGain;
+      outputs[0][i] = output.Left();
+      //right *= gains.x[1] * masterLinearGain;
+      outputs[1][i] = output.Right();
+
+
+#ifdef SELECTABLE_OUTPUT_STREAM_SUPPORT
+      if (IsGuiVisible())
+      {
+        mInputAnalysis[0].WriteSample(inputs[0][i]);
+        mInputAnalysis[1].WriteSample(inputs[1][i]);
+        mOutputAnalysis[0].WriteSample(outputs[0][i]);
+        mOutputAnalysis[1].WriteSample(outputs[1][i]);
+
+        // Feed stereo imaging analysis
+        mInputImagingAnalysis.WriteStereoSample(inputs[0][i], inputs[1][i]);
+        mOutputImagingAnalysis.WriteStereoSample(outputs[0][i], outputs[1][i]);
+      }
+#endif  // SELECTABLE_OUTPUT_STREAM_SUPPORT
+    }
+  }
 };
-
-		M7::MoogOnePoleFilter mFilter;
-
-#ifdef SELECTABLE_OUTPUT_STREAM_SUPPORT
-		AnalysisStream mInputAnalysis[2];
-		AnalysisStream mOutputAnalysis[2];
-		StereoImagingAnalysisStream mInputImagingAnalysis;
-		StereoImagingAnalysisStream mOutputImagingAnalysis;
-#endif // SELECTABLE_OUTPUT_STREAM_SUPPORT
-
-		Maj7Width() :
-			Device((int)ParamIndices::NumParams, mParamCache, gParamDefaults),
-			mParams(mParamCache, 0)
-#ifdef SELECTABLE_OUTPUT_STREAM_SUPPORT
-			,
-			// quite fast moving here because the user's looking for transients.
-			mInputAnalysis{ AnalysisStream{1200}, AnalysisStream{1200} },
-			mOutputAnalysis{ AnalysisStream{1200}, AnalysisStream{1200} }
-#endif // SELECTABLE_OUTPUT_STREAM_SUPPORT
-		{
-			LoadDefaults();
-		}
-
-#ifdef MAJ7WIDTH_FULL_FEATURE
-		// 2D rotation around origin
-		void Rotate2(float& l, float& r, ParamIndices param) {
-			// https://www.musicdsp.org/en/latest/Effects/255-stereo-field-rotation-via-transformation-matrix.html
-			//r = rotation_angle
-			float rot = mParams.GetScaledRealValue(param, -M7::math::gPIHalf, M7::math::gPIHalf, 0);
-			float s = M7::math::sin(rot);
-			float c = M7::math::cos(rot);
-			float t = l * c - r * s;
-			r = l * s + r * c;
-			l = t;
-		}
-#endif // MAJ7WIDTH_FULL_FEATURE
-
-		virtual void Run(float** inputs, float** outputs, int numSamples) override
-		{
-			auto panGains = M7::math::PanToFactor(mParams.GetN11Value(ParamIndices::Pan, 0));
-			mFilter.SetParams(M7::FilterCircuit::OnePole, M7::FilterSlope::Slope6dbOct,
-                        M7::FilterResponse::Highpass,
-                        mParams.GetFrequency(ParamIndices::SideHPFrequency, M7::gFilterFreqConfig),
-                        M7::Param01{0} /*reso*/,
-                        0 /*gain*/);
-			float masterLinearGain = mParams.GetLinearVolume(ParamIndices::OutputGain, gVolumeCfg) * M7::math::gPanCompensationGainLin;
-
-			for (size_t i = 0; i < (size_t)numSamples; ++i)
-			{
-				float l = inputs[0][i];
-				float r = inputs[1][i];
-
-				float left = M7::math::lerp(l, r, mParamCache[(size_t)ParamIndices::LeftSource]);
-				float right = M7::math::lerp(l, r, mParamCache[(size_t)ParamIndices::RightSource]);
-
-#ifdef MAJ7WIDTH_FULL_FEATURE
-				Rotate2(left, right, ParamIndices::RotationAngle);
-#endif // MAJ7WIDTH_FULL_FEATURE
-
-				auto ms = M7::FloatPair{left, right}.MSEncode();
-
-				// hp on side channel
-				ms.x[1] = mFilter.ProcessSample(ms.Side());
-				float msbal = mParams.GetN11Value(ParamIndices::MidSideBalance, 0);
-				if (msbal < 0) {
-					// reduce side
-					ms.x[1] *= msbal + 1.0f;
-				}
-				if (msbal > 0) {
-					ms.x[0] *= 1.0f - msbal;
-				}
-
-				auto outputPrePan = ms.MSDecode() * masterLinearGain;
-				auto output = outputPrePan.mul(panGains);
-
-				//M7::MSDecode(mid, side, &left, &right);
-				//left *= gains.x[0] * masterLinearGain;
-				outputs[0][i] = output.Left();
-				//right *= gains.x[1] * masterLinearGain;
-				outputs[1][i] = output.Right();
-
-
-#ifdef SELECTABLE_OUTPUT_STREAM_SUPPORT
-				if (IsGuiVisible())
-				{
-					mInputAnalysis[0].WriteSample(inputs[0][i]);
-					mInputAnalysis[1].WriteSample(inputs[1][i]);
-					mOutputAnalysis[0].WriteSample(outputs[0][i]);
-					mOutputAnalysis[1].WriteSample(outputs[1][i]);
-					
-					// Feed stereo imaging analysis
-					mInputImagingAnalysis.WriteStereoSample(inputs[0][i], inputs[1][i]);
-					mOutputImagingAnalysis.WriteStereoSample(outputs[0][i], outputs[1][i]);
-				}
-#endif // SELECTABLE_OUTPUT_STREAM_SUPPORT
-
-			}
-		}
-	};
-}
-
+}  // namespace WaveSabreCore
